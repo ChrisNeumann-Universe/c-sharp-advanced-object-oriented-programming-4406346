ddb4aefa4289b82620ebb22bb5d8bfc51c545185
ChrisNeumann <christian.neumann@universe-control.com>
2025-11-19 10:26:16 +0100

    ch1p2 class

--- Start/Ch1/PropsMethods/Program.cs
+++ Start/Ch1/PropsMethods/Program.cs
@@ -1,13 +1,17 @@
 ﻿// Example file for Advanced C#: Object Oriented Programming by Joe Marini
 
 // Create some rectangle objects
-Rectangle rect1 = new Rectangle(10,20);
+Rectangle rect1 = new Rectangle(10, 20);
 Rectangle rect2 = new Rectangle(30);
 
 Console.WriteLine(rect1.GetArea());
 Console.WriteLine(rect2.GetArea());
 
 // TODO: Operate on the Properties
-
+rect1.BorderSize = 5;
+Console.WriteLine($"{rect1.BorderSize}");
+rect1.Width = 5;
+rect1.Height = -30;
+Console.WriteLine(rect1.GetArea());
 
 // TODO: Try setting an invalid value
