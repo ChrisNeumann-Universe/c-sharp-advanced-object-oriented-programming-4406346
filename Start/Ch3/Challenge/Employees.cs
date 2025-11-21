3c4fd5592ee8069d396477b9780e1d761ef8612e
ChrisNeumann <christian.neumann@universe-control.com>
2025-11-21 18:51:39 +0100

    ch3p4 challenge

--- Start/Ch3/Challenge/Employees.cs
+++ Start/Ch3/Challenge/Employees.cs
@@ -1,22 +1,38 @@
 // Example file for Advanced C#: Object Oriented Programming by Joe Marini
 // Solution to the Employee Class challenge
 
-public class Employee {
-    public Employee() {}
+public class Employee
+{
 
-    public required int ID {get; init;}
-    public required string Department {get; set;}
-    public required string FullName {get; set;}
+    static Employee()
+    {
+        EmployeeCount = 0;
+        nextID = 1000;
+    }
+    public Employee()
+    {
+        this.ID = nextID;
+        nextID++;
+        EmployeeCount++;
+    }
 
-    public virtual void AdjustPay(decimal percentage) {}
+    public static int EmployeeCount { get; set; }
+    private static int nextID;
+
+    private int ID { get; init; }
+    public required string Department { get; set; }
+    public required string FullName { get; set; }
+
+    public virtual void AdjustPay(decimal percentage) { }
 
     public override string ToString() => $"{ID}:{FullName}, {Department} ";
 }
 
-public class HourlyEmployee : Employee {
-    public HourlyEmployee() {}
+public class HourlyEmployee : Employee
+{
+    public HourlyEmployee() { }
 
-    public decimal PayRate {get; set;}
+    public decimal PayRate { get; set; }
 
     public override void AdjustPay(decimal percentage)
     {
@@ -24,12 +40,14 @@ public class HourlyEmployee : Employee {
     }
 }
 
-public class SalariedEmployee : Employee {
-    public SalariedEmployee() {}
+public class SalariedEmployee : Employee
+{
+    public SalariedEmployee() { }
 
-    public decimal Salary {get; set;}
+    public decimal Salary { get; set; }
 
-    public override void AdjustPay(decimal percentage) {
+    public override void AdjustPay(decimal percentage)
+    {
         Salary += (Salary * percentage);
     }
 }
